a27d2eb33015d738b1deb062a5a5f9060659f5a6
Gatoreno <erp.reno@hotmail.com>
2021-12-21 04:11:47 -0800

    Agregar archivos de proyecto.

---
+++ Test Foonkie/Test Foonkie/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("DMSerifDisplay-Italic.ttf", Alias = "DMSerifItalic")]
+[assembly: ExportFont("DMSerifDisplay-Regular.ttf", Alias = "DMSerifRegular")]
+[assembly: ExportFont("Rubik-Light.ttf", Alias = "Rubik")]
---
+++ Test Foonkie/Test Foonkie/Views/CustomButtonView.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test_Foonkie.Views
+{
+   [XamlCompilation(XamlCompilationOptions.Compile)]
+   public partial class CustomButtonView : ContentView
+   {
+      public CustomButtonView()
+      {
+         InitializeComponent();
+      }
+
+      public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(CustomButtonView), default(string), propertyChanged: TitleTextPropertyChanged);
+
+      private static void TitleTextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+      {
+         var control = (CustomButtonView)bindable;
+         control.button.Text = newValue.ToString();
+      }
+
+      public string Text
+      {
+         get => (string)GetValue(TextProperty);
+         set => SetValue(TextProperty, value);
+      }
+   }
+}
